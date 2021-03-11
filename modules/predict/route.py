7493173f43ed0917c7e88b66958d6df69a6061dc
Nathan Botelho <nimbotelho@gmail.com>
2021-03-10 23:24:16 -0300

    feat: integrating new app models to predict route

--- modules/predict/route.py
+++ modules/predict/route.py
@@ -1,13 +1,18 @@
 from helpers.http.server import Server
+from models.emotion import EmotionModel
+from models.dataset import DatasetModel
+from models.text import TextModel
 from modules.predict.controller import PredictController
 from modules.predict.service import PredictService
-from models.emotion_test import EmotionTestModel
-from models.emotion_train import EmotionTrainModel
 
-emotion_test_model = EmotionTestModel()
-emotion_train_model = EmotionTrainModel()
+test_dataset_model = DatasetModel('test', 'txt')
+train_dataset_model = DatasetModel('train', 'txt')
 
-predict_service = PredictService(emotion_test_model, emotion_train_model)
+emotion_model = EmotionModel()
+text_model = TextModel()
+
+predict_service = PredictService(test_dataset_model, train_dataset_model, emotion_model, text_model)
+
 predict_controller = PredictController(predict_service)
 
 app = Server.getApp()
--- modules/predict/service.py
+++ modules/predict/service.py
@@ -1,17 +1,24 @@
 class PredictService:
 
-    def __init__(self, emotion_test_model, emotion_train_model):
-        self.__emotion_test_model = emotion_test_model
-        self.__emotion_train_model = emotion_train_model
-
-    def readTrainingData(self):
-        return {
-            'test': self.__emotion_test_model.read(),
-            'train': self.__emotion_train_model.read(),
-        }
-
-    def serializeTrainingData(self, trainingData):
-        pass
+    def __init__(self, test_dataset_model, train_dataset_model, emotion_model, text_model):
+        self.__test_dataset_model = test_dataset_model
+        self.__train_dataset_model = train_dataset_model
+        self.__emotion_model = emotion_model
+        self.__text_model = text_model
 
     def getTrainingModel(self):
+        test_dataset_model = self.__test_dataset_model
+        train_dataset_model = self.__train_dataset_model
+        emotion_model = self.__emotion_model
+        text_model = self.__text_model
+
+        test_dataset = test_dataset_model.read()
+        train_dataset = train_dataset_model.read()
+
+        print(test_dataset)
+        print(train_dataset)
+
+        return '{'
+
+    def getTextMessage(self, text):
         pass
