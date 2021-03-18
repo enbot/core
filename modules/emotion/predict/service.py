1b90d80b342e9fba431db599701ca77a299b08d9
Nathan Botelho <nimbotelho@gmail.com>
2021-03-18 00:48:31 -0300

    feat: add serializing dataset methods and integrate its models

--- modules/emotion/predict/service.py
+++ modules/emotion/predict/service.py
@@ -7,44 +7,51 @@ class PredictService:
 
     def readTrainingData(self):
         file_dir = '/data/emotionsDatasetForNlp/'
-        test_file_nasme = 'test'
-        train_file_nasme = 'train'
+        test_file_name = 'test'
+        train_file_name = 'train'
         file_extension = '.txt'
 
-        test_dataset = self.__dataset_model.readEmotionDataset(test_file_nasme, file_extension, file_dir)
-        train_dataset = self.__dataset_model.readEmotionDataset(train_file_nasme, file_extension, file_dir) 
+        train_dataset = self.__dataset_model.readEmotionDataset(train_file_name, file_extension, file_dir) 
+        test_dataset = self.__dataset_model.readEmotionDataset(test_file_name, file_extension, file_dir)
 
         return {
-            "test": test_dataset,
             "train": train_dataset,
+            "test": test_dataset,
+        }
+
+    def createTrainingDataset(self, database):
+        train_database = database["train"]
+        test_database = database["test"][20:]
+        # train_database = database["train"][:100]
+        # test_database = database["test"][:20]
+
+        train_base = self.__text_model.applyDatasetModifiers(train_database)
+        test_base = self.__text_model.applyDatasetModifiers(test_database)
+
+        train_unique_words = self.__text_model.getDatasetUniqueWords(train_base)
+        test_unique_words = self.__text_model.getDatasetUniqueWords(test_base)
+
+        complete_train_dataset = self.__emotion_model.createTrainingDataset(train_base, train_unique_words)
+        complete_test_dataset = self.__emotion_model.createTrainingDataset(test_base, test_unique_words)
+
+        return {
+            "train": complete_train_dataset,
+            "test": complete_test_dataset,
+            "words": train_unique_words,
         }
 
     def createTrainingModel(self, datasets):
         train_dataset = datasets["train"]
         test_dataset = datasets["test"]
 
-        train_base = self.__text_model.applyModifiers(train_dataset)
-        test_base = self.__text_model.applyModifiers(test_dataset)
+        training_model = self.__emotion_model.createTrainingModel(train_dataset, test_dataset)
 
-        train_unique_words = self.__text_model.getUniqueWords(train_base)
-        test_unique_words = self.__text_model.getUniqueWords(test_base)
+        return training_model
 
-        complete_train_dataset = self.__emotion_model.getTrainingDataset(train_base, train_unique_words)
-        complete_test_dataset = self.__emotion_model.getTrainingDataset(test_base, test_unique_words)
+    def createInputMessage(self, input_message, datasets):
+        train_dataset = datasets["words"]
 
-        # train_dataframe = self.__emotion_model.createDataframe(train_base)
-        # test_dataframe = self.__emotion_model.createDataframe(test_base)
+        train_message = self.__text_model.applyTextModifiers(input_message)
+        train_input = self.__emotion_model.createTrainingInput(train_message, train_dataset)
 
-        # print(train_unique_words)
-        # print(test_base)
-        # print(train_base)
-
-        # print(train_unique_words)
-        # print(test_unique_words)
-
-        print("soon")
-
-        return 'soon'
-
-    def createInputMessage(self, text):
-        pass
+        return train_input
