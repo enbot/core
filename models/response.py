8a48a83d2663906c7501073a775b94277e56eb5e
Nathan Botelho <nimbotelho@gmail.com>
2021-04-14 23:27:56 -0300

    feat: finish response model

--- models/response.py
+++ models/response.py
@@ -4,16 +4,16 @@ import aiml
 class ResponseModel:
 
     def __init__(self):
-        self.__last_emotion = None
+        self.__current_emotion = None
         self.__kernel = aiml.Kernel()
         self.__kernel.bootstrap(learnFiles=['aiml/start.xml'], commands=['load aiml b'])
 
-    def getLastEmotion(self):
-        return self.__last_emotion
+    def getCurrentEmotion(self):
+        return self.__current_emotion
 
     def changeEmotion(self, emotion):
-        # self.__kernel.respond()
-        pass
+        self.__kernel.respond('turn your emotion into {}'.format(emotion))
+        self.__current_emotion = emotion
 
     def getResponse(self, message):
         self.__kernel.respond(message)
