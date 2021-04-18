936887fd43b3b9982cfd1d1bb610b517d8ece9b3
Nathan Botelho <nimbotelho@gmail.com>
2021-04-18 19:06:13 -0300

    chore: update project name and its dependencies

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='emotions',
-    packages=['emotions'],
+    name='core',
+    packages=['core'],
     include_package_data=True,
     install_requires=[
-        'flask',
-        'pandas',
-        'numpy',
-        'matplotlib',
         'nltk',
         'flask',
+        'python-aiml',
     ],
 )
