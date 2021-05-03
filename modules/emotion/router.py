efcae10d11fb834573c22379b65192e78bfb49b8
Nathan Botelho <nimbotelho@gmail.com>
2021-05-02 21:41:06 -0300

    feat: integrate new emotion calculate route to emotion router file

--- modules/emotion/router.py
+++ modules/emotion/router.py
@@ -1,15 +1,18 @@
+from modules.emotion.calculate.factory import EmotionCalculateFactory
 from modules.emotion.predict.factory import EmotionPredictFactory
 
 
 class EmotionRouter:
 
     def __init__(self):
-        self.__emotionController = EmotionPredictFactory.assemble()
+        self.__emotionCalculateFactory = EmotionCalculateFactory.assemble()
+        self.__emotionPredictController = EmotionPredictFactory.assemble()
 
     def router(self):
         return {
             "module": "emotion",
             "routes": [
-                { "method" : "POST", "route" : "/emotions", "handler": self.__emotionController.handle }
+                { "method" : "POST", "route" : "/emotion/calculate", "handler": self.__emotionCalculateFactory.handle },
+                { "method" : "POST", "route" : "/emotion/predict", "handler": self.__emotionPredictController.handle },
             ]
         }
