0cafb932717d4936017d71222d1c46d710c4790c
Nathan Botelho <nimbotelho@gmail.com>
2021-05-04 19:18:40 -0300

    feat: setup cors to application server

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ from helpers.config.server import Server
 server = Server()
 
 server.startRoutes()
+server.setupCors()
 server.startApp()
--- helpers/config/server.py
+++ helpers/config/server.py
@@ -1,3 +1,4 @@
+from flask_cors import CORS
 from flask import Blueprint
 from flask import Flask
 from modules.emotion.router import EmotionRouter
@@ -20,6 +21,9 @@ class Server:
             port=5000,
         )
 
+    def setupCors(self):
+        CORS(self.__app, resources={r"*": {"origins": "*"}})
+
     def startRoutes(self):
         app = self.__app
         modules = self.__modules
@@ -31,4 +35,4 @@ class Server:
                 route = router["routes"][index]
                 blueprint = Blueprint(router["module"] + str(index), __name__)
                 blueprint.route(route["route"], methods=[route["method"]])(route["handler"])
-                app.register_blueprint(blueprint, url_prefix='/api')
+                app.register_blueprint(blueprint, url_prefix="/api")
